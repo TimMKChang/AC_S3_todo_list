1e5816eda73d1b3be0b737915b81222ac96d6518
Tim Chang <b00501007@gmail.com>
2020-02-25 18:27:22 +0800

    Add new template, create feature, and install body-parser

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/AC_S3_todo', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -25,28 +28,36 @@ const Todo = require('./models/todo')
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
-    .exec((err, todos) => {
-      if (err) {
-        return console.error(err)
-      }
+    .then(todos => {
       return res.render('index', { todos })
+    })
+    .catch(err => {
+      return console.error(err)
     })
 })
 // show all todos
 app.get('/todos', (req, res) => {
-  res.redirect('/')
+  return res.redirect('/')
+})
+// create todo page
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
 })
 // get one todo
 app.get('/todos/:id', (req, res) => {
   res.send('show one todo')
 })
-// create todo page
-app.get('/todos/new', (req, res) => {
-  res.send('create todo page')
-})
 // create todo
 app.post('/todos', (req, res) => {
-  res.send('create todo')
+  const todo = new Todo({
+    name: req.body.name
+  })
+  todo.save(err => {
+    if (err) {
+      return console.error(err)
+    }
+    return res.redirect('/')
+  })
 })
 // update todo page
 app.get('/todos/:id/edit', (req, res) => {
