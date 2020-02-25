6a08e197ffa586a7c18a53c49d5653112bbc4761
Tim Chang <b00501007@gmail.com>
2020-02-25 16:57:08 +0800

    Add routes

--- app.js
+++ app.js
@@ -17,8 +17,37 @@ db.once('open', () => {
 const Todo = require('./models/todo')
 
 // setting routes
+// homepage
 app.get('/', (req, res) => {
   res.send('homepage')
+})
+// show all todos
+app.get('/todos', (req, res) => {
+  res.send('show all todos')
+})
+// get one todo
+app.get('/todos/:id', (req, res) => {
+  res.send('show one todo')
+})
+// create todo page
+app.get('/todos/new', (req, res) => {
+  res.send('create todo page')
+})
+// create todo
+app.post('/todos', (req, res) => {
+  res.send('create todo')
+})
+// update todo page
+app.get('/todos/:id/edit', (req, res) => {
+  res.send('update todo page')
+})
+// update todo
+app.post('/todos/:id/edit', (req, res) => {
+  res.send('update todo')
+})
+// delete todo
+app.post('/todos/:id/delete', (req, res) => {
+  res.send('delete todo')
 })
 
 app.listen(port, () => {
